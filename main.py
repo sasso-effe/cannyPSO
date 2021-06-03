5af75278d63dd75c49d91c82eab8096c3661bf0c
Sasso <pietro.peppo@gmail.com>
2021-06-03 11:16:24 +0200

    Multiple images as training set

--- main.py
+++ main.py
@@ -63,109 +63,33 @@ class Plane:
                 self.best_pos = np.copy(p.best_pos)
 
 
-def canny(img, gaussianKernel=(5, 5)):
-    """
-    Compute Canny's edge detector
-    without the final double thresholding step
-    """
-    m, n = img.shape
-    # noise reduction
-    img = cv2.GaussianBlur(img, gaussianKernel, 0)
-    # gradients
-    gx = cv2.Sobel(np.float32(img), cv2.CV_64F, 1, 0, 3)
-    gy = cv2.Sobel(np.float32(img), cv2.CV_64F, 0, 1, 3)
-    # polar coordinates
-    magnitude, angle = cv2.cartToPolar(gx, gy, angleInDegrees=True)
-    # iterate over pixels
-    for i in range(1, m - 1):
-        for j in range(1, n - 1):
-            grad_ang = angle[i, j]
-            grad_ang = abs(grad_ang - 180) if abs(grad_ang) > 180 else abs(grad_ang)
-
-            # select the neighbourhood of the current pixel according grad direction
-            # direction: horizontal -
-            if grad_ang <= 22.5 or (157.5 < grad_ang <= 180):
-                start_i, start_j = i, j - 1
-                end_i, end_j = i, j + 1
-
-            # direction: diagonal /
-            elif 22.5 < grad_ang <= 67.5:
-                start_i, start_j = i - 1, j - 1
-                end_i, end_j = i + 1, j + 1
-
-            # direction: vertical |
-            elif 67.5 < grad_ang <= 112.5:
-                start_i, start_j = i - 1, j
-                end_i, end_j = i + 1, j
-
-            # direction: diagonal \
-            elif 112.5 < grad_ang <= 157.5:
-                start_i, start_j = i - 1, j + 1
-                end_i, end_j = i + 1, j - 1
-
-            # Non-maximum suppression step
-            if magnitude[i, j] < magnitude[start_i, start_j] \
-                    or magnitude[i, j] < magnitude[end_i, end_j]:
-                magnitude[i, j] = 0
-
-    return magnitude
-
-
-def double_thresh(img, tl, th):
-    img = np.copy(img)
-    m, n = img.shape
-    for i in range(m):
-        for j in range(n):
-            if img[i, j] < tl:
-                img[i, j] = 0
-            elif th > img[i, j]:
-                img[i, j] = 100
-            else:
-                img[i, j] = 150
-    return img
-
-
-def hysteresis(img):
-    # double thresholding step
-    img = np.copy(img)
-    m, n = img.shape
-    # hysteresis (naive approach)
-    for i in range(1, m - 1):
-        for j in range(1, n - 1):
-            if img[i, j] == 100:
-                if ((img[i + 1, j - 1] == 150) or (img[i + 1, j] == 150) or (img[i + 1, j + 1] == 150)
-                        or (img[i, j - 1] == 150) or (img[i, j + 1] == 150)
-                        or (img[i - 1, j - 1] == 150) or (img[i - 1, j] == 150) or (
-                                img[i - 1, j + 1] == 150)):
-                    img[i, j] = 150
-                else:
-                    img[i, j] = 0
-    return img
-
-
 def obj_function(img, pos, goal, lut):
     tl, th = pos
     if lut[tl][th] < 0:
-        edges = cv2.Canny(img, tl, th)
-        diff = edges - goal
-        f = np.sum(np.abs(diff))
+        f = 0
+        for (i, g) in zip(img, goal):
+            edges = cv2.Canny(i, tl, th)
+            diff = edges - g
+            f += np.sum(np.abs(diff)) * 1.0 / (diff.shape[0] * diff.shape[1])
         lut[tl][th] = f
         return f
     return lut[tl][th]
 
 
-def pso(img, goal, max_iterations, tol, tmax):
-    plane = Plane(50, img, goal, w=0.5, c1=0.8, c2=0.9, tmax=tmax)
+def pso(imgs, goals, max_iterations, tol, tmax):
+    plane = Plane(50, imgs, goals, w=0.5, c1=0.8, c2=0.9, tmax=tmax)
     plt.ion()
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(18, 6))
+    fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(18, 6))
     positions = np.array(list(map(lambda x: x.pos, plane.particles)))
     ax1.set_xlim([0, plane.tmax])
     ax1.set_ylim([0, plane.tmax])
     scat = ax1.scatter([], [])
     scat.set_offsets(positions)
     tl, th = plane.best_pos
-    ax2.imshow(cv2.Canny(img, tl, th), cmap="gray")
-    ax3.imshow(goal, cmap="gray")
+    ax2.imshow(cv2.Canny(imgs[0], tl, th), cmap="gray")
+    ax3.imshow(goals[0], cmap="gray")
+    ax5.imshow(cv2.Canny(imgs[1], tl, th), cmap="gray")
+    ax6.imshow(goals[1], cmap="gray")
     plt.show()
     plt.pause(0.05)
     for i in range(max_iterations):
@@ -173,17 +97,17 @@ def pso(img, goal, max_iterations, tol, tmax):
         positions = np.array(list(map(lambda x: x.pos, plane.particles)))
         positions = np.concatenate([positions, np.array(plane.best_pos, ndmin=2)])
         colors = np.array(['C0' for i in range(positions.shape[0] - 1)] + ['C1'])
-        print(colors)
         scat.set_offsets(positions)
         scat.set_facecolors(colors)
         tl, th = plane.best_pos
-        ax2.imshow(cv2.Canny(img, tl, th), cmap="gray")
+        ax2.imshow(cv2.Canny(imgs[0], tl, th), cmap="gray")
+        ax5.imshow(cv2.Canny(imgs[1], tl, th), cmap="gray")
         print("Current best = " + str(plane.best_value) + " at position " + str(plane.best_pos))
-        plt.pause(0.02)
+        plt.pause(0.01)
         plt.savefig("{0}.png".format(i))
         if plane.best_value < tol:
             break
-    print("finished after " + str(i+1) + " iterations")
+    print("finished after " + str(i + 1) + " iterations")
     print("position: " + str(plane.best_pos))
     print("value: " + str(plane.best_value))
 
@@ -195,8 +119,15 @@ def pso(img, goal, max_iterations, tol, tmax):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    image = cv2.imread("res/chessboard1.jpeg", cv2.IMREAD_GRAYSCALE)
-    image = cv2.GaussianBlur(image, (5, 5), 0)
-    tmax = int(1000)
-    g = cv2.Canny(image, 80, 530)
-    pso(image, g, 50, 1e-10, tmax)
+    img_names = ["chessboard1.jpeg", "chessboard2.jpg"]
+    thresholds = [[80, 530], [80, 500]]
+    tmax = 1000
+
+    images = []
+    goals = []
+    for n, th in zip(img_names, thresholds):
+        img = cv2.imread("res/{0}".format(n), cv2.IMREAD_GRAYSCALE)
+        img = cv2.GaussianBlur(img, (5, 5), 0)
+        images.append(img)
+        goals.append(cv2.Canny(img, th[0], th[1]))
+    pso(images, goals, 20, 1e-10, tmax)
